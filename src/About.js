53d1b65520272abf39db97c67a50989febc72c52
TCG <ssibonokuhle@gmail.com>
2021-02-25 09:42:45 +0200

    Added table

--- src/About.js
+++ src/About.js
@@ -5,8 +5,36 @@ import "./App.css";
 function About() {
   return (
     <div>
-      <h1>About page</h1>
+      <h1>Form register</h1>
+      <p>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Location</th>
+            <th>Email</th>
+          </tr>
+          <tr>
+            <th>Kuhle</th>
+            <th>Nkabinde</th>
+            <th>Riverside</th>
+            <th>Kuhle@gmail.com</th>
+          </tr>
+          <tr>
+            <th>Sizwe</th>
+            <th>Zwane</th>
+            <th>Riverside</th>
+            <th>mszweesi@gmail.com</th>
+          </tr>
+          <tr>
+            <th>Kuhle</th>
+            <th>Nkabinde</th>
+            <th>Riverside</th>
+            <th>Kuhle@gmail.com</th>
+          </tr>
+        </table>
+      </p>
     </div>
   );
 }
-export default App;
+export default About;
