e58b4f89ae22e38a889d4ac8216cd804d7b20402
TCG <ssibonokuhle@gmail.com>
2021-02-26 10:42:36 +0200

    Created an file of itemDetails

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -1,30 +1,30 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+// import React, { useState, useEffect } from "react";
+// import "./App.css";
 
-function Shop({ match }) {
-  const fetchItems = async () => {
-    const fetchItem = await fetch(
-      `http://fornite-api.theapinetwork.com/prod09/item/get?ids=${match.params.id}`
-    );
-    const item = await fetchItem.json();
-    setItem(item);
-    console.log(item);
-  };
-  useEffect(() => {
-    fetchItems();
+// function Shop({ match }) {
+//   const fetchItems = async () => {
+//     const fetchItem = await fetch(
+//       `http://fornite-api.theapinetwork.com/prod09/item/get?ids=${match.params.id}`
+//     );
+//     const item = await fetchItem.json();
+//     setItem(item);
+//     console.log(item);
+//   };
+//   useEffect(() => {
+//     fetchItems();
 
-    console.log(match);
-  }, []);
+//     console.log(match);
+//   }, []);
 
-  const [item, setItem] = useState({
-    images: {},
-  });
+//   const [item, setItem] = useState({
+//     images: {},
+//   });
 
-  return (
-    <div>
-      <h1>{item.name}</h1>
-      <img src={item.images.transparent} alt="" />
-    </div>
-  );
-}
-export default Shop;
+//   return (
+//     <div>
+//       <h1>{item.name}</h1>
+//       <img src={item.images.transparent} alt="" />
+//     </div>
+//   );
+// }
+// export default Shop;
